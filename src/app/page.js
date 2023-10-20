319c15f02f5cf0772c7bd915873767552075e719
Alex Nascimento <alexdavi150@gmail.com>
2023-10-20 12:08:20 -0300

    feat: add responsiveness size to page title

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import VehicleCard from "@/components/vehicleCard"
 export default function Home() {
   return (
     <main className='container mx-auto'>
-      <h1 className="text-3xl font-semibold mt-28">Available vehicles</h1>
+      <h1 className="text-3xl md:text-4xl font-semibold mt-28">Available vehicles</h1>
 
       <div className="bg-gray-800 mt-2 py-4 px-3 rounded-xl flex flex-col gap-4 max-h-[35rem] overflow-y-scroll">
         <VehicleCard />
