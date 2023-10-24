18e11a94457d5e002ec7e54f0b2fe843be57b174
Alex Nascimento <alexdavi150@gmail.com>
2023-10-24 17:19:44 -0300

    feat: use props for the data

--- src/components/vehicleCard.jsx
+++ src/components/vehicleCard.jsx
@@ -1,16 +1,41 @@
+'use client'
+
 import React from 'react'
 
-function VehicleCard() {
+function VehicleCard({
+  id,
+  model,
+  brand,
+  color,
+  price
+}) {
+  async function deleteVehicle() {
+    await fetch(`${process.env.API_URL}/api/vehicle/${id}`, {
+      method: 'DELETE'
+    })
+
+    window.location.reload()
+  }
+
+  function handleClick() {
+    window.location.replace('/editVehicle/' + id)
+  }
+
   return (
-    <div className='bg-sky-700 rounded-md p-2 grid grid-cols-8 text-center text-xl sm:text-2xl xl:text-3xl text-white font-semibold items-center'>
+    <div className='bg-sky-700 rounded-md p-2 grid grid-cols-8 text-center text-xl sm:text-2xl xl:text-3xl text-white font-semibold items-center cursor-pointer'
+      onClick={handleClick}
+    >
       <div className='col-span-8 sm:col-span-5 sm:text-left'>
-        <h2 className=''>Vehicle Model • vehicle color</h2>
-        <p className='text-stone-400 xl:text-2xl'>Vehicle Brand</p>
+        <h2 className=''>{model} • {color}</h2>
+        <p className='text-stone-400 xl:text-2xl'>{brand}</p>
       </div>
 
-      <h2 className='col-span-8 sm:col-span-2'>999.999,99</h2>
+      <h2 className='col-span-8 sm:col-span-2'>{price}</h2>
 
-      <h2 className='col-span-8 sm:col-span-1'>Icon</h2>
+      <h2
+        className='col-span-8 sm:col-span-1 cursor-pointer hover:text-red-500 duration-300'
+        onClick={deleteVehicle}
+      >DELETE</h2>
     </div>
   )
 }
