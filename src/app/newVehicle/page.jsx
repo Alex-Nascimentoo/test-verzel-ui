4f7a05542eea5fecc22c3fb811d7fc387ac23322
Alex Nascimento <alexdavi150@gmail.com>
2023-10-24 17:16:45 -0300

    feat: create new vehicle page

--- src/app/newVehicle/page.jsx
+++ src/app/newVehicle/page.jsx
@@ -1,40 +1,19 @@
 import React from 'react'
 
-function Page() {
+import ClientPage from './clientPage'
+
+async function Page() {
+  var vehicleList = await fetch(`${process.env.API_URL}/api/vehicle`, {
+    method: 'GET',
+    headers: {
+      "accept": "application/json",
+    }
+  }).then(data => data.json())
+
   return (
-    <main className='container mx-auto'>
-      <h1 className='text-3xl md:text-4xl font-semibold mt-28'>Create new vehicle</h1>
-
-      <form
-        action=""
-        className='mx-auto p-3 bg-gray-800 rounded-xl max-w-xl grid grid-cols-1 sm:grid-cols-2 gap-4 mt-5'
-      >
-        <div className='flex flex-col'>
-          <label className='text-white text-xl' htmlFor="">Model</label>
-          <input className='rounded-md p-1 px-2 focus:outline-none' type="text" placeholder='Cayenne' name="" id="" />
-        </div>
-
-        <div className='flex flex-col'>
-          <label className='text-white text-xl' htmlFor="">Brand</label>
-          <input className='rounded-md p-1 px-2 focus:outline-none' type="text" placeholder='Porsche' name="" id="" />
-        </div>
-
-        <div className='flex flex-col mt-2'>
-          <label className='text-white text-xl' htmlFor="">Type</label>
-          <select className='rounded-md p-1 px-2 focus:outline-none' name="" id="">
-            <option value="">Car</option>
-            <option value="">Motorcycle</option>
-            <option value="">Truck</option>
-            <option value="">Aircraft</option>
-          </select>
-        </div>
-
-        <div className='flex flex-col mt-2'>
-          <label className='text-white text-xl' htmlFor="">Price</label>
-          <input className='rounded-md p-1 px-2 focus:outline-none' type="number" placeholder='999.999,99' name="" id="" />
-        </div>
-      </form>
-    </main>
+    <ClientPage 
+      vehicleList={vehicleList}
+    />
   )
 }
 
