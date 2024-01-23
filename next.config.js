1e70d90e7e2e905571f0c1efc3551754b40975ef
Alex Nacimento <alexdavi150@gmail.com>
2024-01-22 21:35:15 -0300

    chore: update env variable for the API route

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "http://localhost:5000",
+    API_URL: "http://localhost:5000/api",
     ADMIN_PASSWORD: "bolinho"
   },
   images: {
